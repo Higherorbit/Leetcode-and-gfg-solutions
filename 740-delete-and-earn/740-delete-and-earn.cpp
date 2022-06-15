28e51c91f8ae9b711862248d0db133349c905d04
Samandeep Singh <87420218+Higherorbit@users.noreply.github.com>
2022-06-15 18:16:36 +0530

    Time: 24 ms (44.79%), Space: 13.5 MB (55.80%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.cpp
+++ 740-delete-and-earn/740-delete-and-earn.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     int deleteAndEarn(vector<int>& v) {
         int m=*max_element(v.begin(),v.end());
-        int dp[m+1]        ;
+        int dp[m+1];
         memset(dp,0,sizeof(dp));
-        map<int,int> count;
+        unordered_map<int,int> count;
         for(int i=0;i<v.size();i++){
             count[v[i]]++;
         }
