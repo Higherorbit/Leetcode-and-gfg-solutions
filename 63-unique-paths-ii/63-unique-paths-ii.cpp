339595b4fc064b7f9c85fc51263bb4dbd5a5449f
Samandeep Singh <87420218+Higherorbit@users.noreply.github.com>
2022-06-19 18:59:30 +0530

    Time: 8 ms (25.79%), Space: 7.8 MB (42.74%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -24,12 +24,12 @@ public:
                 dp[i][j]=dp[i-1][j]+dp[i][j-1];
             }
         }
-        for(int i=0;i<=n;i++){
-            for(int j=0;j<=m;j++){
-                cout<<dp[i][j]<<" ";
-            }
-            cout<<endl;
-        }
+        // for(int i=0;i<=n;i++){
+        //     for(int j=0;j<=m;j++){
+        //         cout<<dp[i][j]<<" ";
+        //     }
+        //     cout<<endl;
+        // }
         return dp[n][m];
     }
 };
