1376b9c18100e289c8a17a4fae91b56648ba36fe
Samandeep Singh <87420218+Higherorbit@users.noreply.github.com>
2022-06-29 22:27:00 +0530

    Time: 3 ms (61.60%), Space: 6.7 MB (31.49%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.cpp
+++ 377-combination-sum-iv/377-combination-sum-iv.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int combinationSum4(vector<int>& v, int sum) {
-            #define int __int128_t
+            #define int unsigned int
         int n=v.size();
         sort(v.begin(),v.end());
         vector<int>dp(sum+1,0);
