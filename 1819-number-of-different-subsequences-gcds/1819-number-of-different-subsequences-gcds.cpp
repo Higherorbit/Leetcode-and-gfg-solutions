ffdabf30418a411d924d8835b97193258e2d9d2c
Samandeep Singh <87420218+Higherorbit@users.noreply.github.com>
2022-07-11 18:45:08 +0530

    Time: 613 ms (45.00%), Space: 72.3 MB (55.83%) - LeetHub

---
+++ 1819-number-of-different-subsequences-gcds/1819-number-of-different-subsequences-gcds.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int countDifferentSubsequenceGCDs(vector<int>& nums) {
+        int n=nums.size();
+        vector<bool> present(200001,false);
+        for(int i=0;i<n;i++){
+            present[nums[i]]=true;
+        }
+        int ans=0;
+        for(int i=1;i<=200000;i++){
+            int ok=-1;
+            if(present[i]) ans++;
+            else{
+                for(int j=i+i;j<=200000;j+=i){
+                    if(!present[j]){
+                        continue;
+                    }
+                    if(ok==-1){
+                        ok=j;                        
+                    }
+                    else{
+                        ok=__gcd(ok,j);
+                    }
+                }
+                if(ok==i){
+                    ans++;
+                }
+            }
+        }
+        return ans;
+        
+    }
+    
+};
