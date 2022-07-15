48a9ab5b1a2bc59a9db15f7dd1876fb0323bdac4
Samandeep Singh <87420218+Higherorbit@users.noreply.github.com>
2022-07-15 10:06:34 +0530

    Added solution - LeetHub

--- Min sum formed by digits - GFG/min-sum-formed-by-digits.cpp
+++ Min sum formed by digits - GFG/min-sum-formed-by-digits.cpp
@@ -8,41 +8,28 @@ class Solution{
     public:
     long long int minSum(int arr[], int n)
     {
-        long long int ans=0;
+        long long int a=0;
+        long long int b=0;
         if(n==1){
             return arr[0];
         }
         sort(arr,arr+n);
-        string s="";
-        string t="";
+        
         bool bb=true;
         for(int i=0;i<n;i++){
             if(bb){
-                s+=to_string(arr[i]);
+                a*=10;
+                a+=arr[i];
                 bb=false;
             }
             else{
-                t+=to_string(arr[i]);
+                b*=10;
+                b+=arr[i];
                 bb=true;
             }
         }
-        long long a=s[s.size()-1]-'0';
-        long long b=t[t.size()-1]-'0';
-        // cout<<a<<endl;
-        // cout<<b<<endl;
-        long long p=10;
-        for(int i=s.size()-2;i>=0;i--){
-            a+=(s[i]-'0')*p;
-            p*=10;
-        }
-        p=10;
-        for(int i=t.size()-2;i>=0;i--){
-            b+=(t[i]-'0')*p;
-            p*=10;
-        }
-        // cout<<b<<endl;
-        ans=a+b;
-        return ans;
+        // cout<<a<<" "<<b<<endl;
+        return a+b;
     }
 };
 
