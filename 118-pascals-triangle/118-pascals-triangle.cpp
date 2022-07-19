09da88cfc0d8a6a83a4662f83ab06e5ac9ca299d
Samandeep Singh <87420218+Higherorbit@users.noreply.github.com>
2022-07-19 11:51:51 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (31.97%) - LeetHub

---
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<int> f(1,1);
+        vector<vector<int>>ans;
+        ans.push_back(f);
+        for(int i=2;i<=numRows;i++){
+            vector<int>temp(i,1);
+            for(int j=1;j<=i-2;j++){
+                temp[j]=f[j-1]+f[j];
+            }
+            f.resize(i);
+            f=temp;
+            ans.push_back(f);
+            
+        }
+        return ans;
+    }
+};
