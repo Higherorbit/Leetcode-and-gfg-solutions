da45544029b5441272060b6b84960cc7f5864488
Samandeep Singh <87420218+Higherorbit@users.noreply.github.com>
2022-07-20 16:38:51 +0530

    Time: 712 ms (40.77%), Space: 8.2 MB (12.94%) - LeetHub

--- 79-word-search/79-word-search.cpp
+++ 79-word-search/79-word-search.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int n;
     int m;
-    bool dfs(vector<vector<char>>& board, string word,vector<vector<bool>>&visited, int i, int j, int ind){
+    bool dfs(vector<vector<char>>& board, string word,vector<vector<bool>>&visited ,int i, int j, int ind){
         //base case
         if(ind==word.size()){
             return true;
@@ -10,23 +10,23 @@ public:
         if(i<0 or j<0 or i>=n or j>=m){
             return false;
         }
-        
         if(visited[i][j] or board[i][j]!=word[ind]){
             return false;
-        }   
+        }
         //main case
         visited[i][j]=true;
-        bool ans=dfs(board,word,visited, i+1,j,ind+1)|
-            dfs(board,word,visited, i-1,j,ind+1)|
-            dfs(board,word,visited, i,j+1,ind+1)|
-            dfs(board,word,visited, i,j-1,ind+1);
+        bool ans=dfs(board,word,visited,i-1,j,ind+1)|
+                dfs(board,word,visited,i+1,j,ind+1)|
+                dfs(board,word,visited,i,j+1,ind+1)|
+                dfs(board,word,visited,i,j-1,ind+1);
+        // backtracking
         visited[i][j]=false;
-        return ans;                
-    }
+        return ans;
+    }   
     bool exist(vector<vector<char>>& board, string word) {
         n=board.size();
         m=board[0].size();
-        vector<vector<bool>>visited(n, vector<bool>(m,false));
+        vector<vector<bool>>visited(n,vector<bool>(m,false));
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(board[i][j]==word[0]){
