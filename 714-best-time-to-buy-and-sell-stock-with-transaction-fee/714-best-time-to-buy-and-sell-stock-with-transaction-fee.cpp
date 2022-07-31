cc782059e856df31fad591a3ccdec02cfecef04e
Samandeep Singh <87420218+Higherorbit@users.noreply.github.com>
2022-07-31 11:43:31 +0530

    Time: 202 ms (58.24%), Space: 55.4 MB (62.71%) - LeetHub

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -6,15 +6,29 @@ public:
         if(n==1){
             return 0;
         }
+        //dp[i][0] means we dont have stock ont that day
+        //dp[i][1] means we have
         dp[0][0]=0;
         dp[0][1]=-v[0];
-        dp[1][0]=max(dp[0][0],dp[0][1]+v[1]-fee);
-        dp[1][1]=max(dp[0][1],-v[1]);
-        for(int i=2;i<n;i++){
+        for(int i=1;i<n;i++){
             dp[i][0]=max(dp[i-1][0],dp[i-1][1]+v[i]-fee);
             dp[i][1]=max(dp[i-1][1],dp[i-1][0]-v[i]);
         }
         return max(dp[n-1][0],dp[n-1][1]);
+        
+        
+        
+        
+        
+        // dp[0][0]=0;
+        // dp[0][1]=-v[0];
+        // dp[1][0]=max(dp[0][0],dp[0][1]+v[1]-fee);
+        // dp[1][1]=max(dp[0][1],-v[1]);
+        // for(int i=2;i<n;i++){
+        //     dp[i][0]=max(dp[i-1][0],dp[i-1][1]+v[i]-fee);
+        //     dp[i][1]=max(dp[i-1][1],dp[i-1][0]-v[i]);
+        // }
+        // return max(dp[n-1][0],dp[n-1][1]);
         
     }
 };
