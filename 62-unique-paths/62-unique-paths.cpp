1222bdbb44208441f83af98b61f17bc205e715d2
Samandeep Singh <87420218+Higherorbit@users.noreply.github.com>
2022-08-01 23:35:58 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (46.14%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
+    vector<vector<int>>dp;
+    int solve(int n, int m){
+        if(n==1 or m==1){
+            return 1;
+        }
+        if(dp[n][m]!=-1){
+            return dp[n][m];
+        }
+        return dp[n][m]=solve(n-1,m)+solve(n,m-1);
+    }
     int uniquePaths(int n, int m) {
-        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
-        for(int j=1;j<=m;j++){
-            dp[1][j]=1;
-        }
-        for(int i=1;i<=n;i++){
-            dp[i][1]=1;
-        }
-        for(int i=2;i<=n;i++){
-            for(int j=2;j<=m;j++){
-                dp[i][j]=dp[i-1][j]+dp[i][j-1];
-            }
-        }
-        return dp[n][m];
-        
+        dp.resize(n+1,vector<int>(m+1,-1));
+        return solve(n,m);
     }
 };
