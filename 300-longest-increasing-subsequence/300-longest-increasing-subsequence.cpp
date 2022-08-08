a0fff2ba48709c072abc274382415330d2db9b18
Samandeep Singh <87420218+Higherorbit@users.noreply.github.com>
2022-08-08 15:30:56 +0530

    Time: 558 ms (29.87%), Space: 10.6 MB (47.88%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
     int lengthOfLIS(vector<int>& v) {
+        //O(n^2)      
+        // at every index we can store the length of long est increeasing subsequence ending at it
         int n=v.size();
-        int dp[n];
+        vector<int>dp(n,1);
         for(int i=0;i<n;i++){
-            dp[i]=1;
-        }
-        for(int i=1;i<n;i++){
-            int cur_max=0;
-            for(int j=i;j>=0;j--){
-                if(v[j]<v[i]){
-                    cur_max=max(cur_max,dp[j]);
+            for(int j=i-1;j>=0;j--){
+                if(v[i]>v[j]){
+                    dp[i]=max(dp[i],dp[j]+1);
                 }
             }
-            dp[i]=max(dp[i],1+cur_max);
         }
         int ans=0;
-        for(int i=0;i<n;i++){
-            ans=max(ans,dp[i]);
+        for(auto &x:dp){
+            ans=max(ans,x);
         }
-        return ans;        
+        return ans;
+        
     }
 };
