e0c9e4d1790a9c00cee92167f33cb95975469276
Samandeep Singh <87420218+Higherorbit@users.noreply.github.com>
2022-08-14 19:24:53 +0530

    Time: 21 ms (42.55%), Space: 6 MB (79.43%) - LeetHub

---
+++ 1012-numbers-with-repeated-digits/1012-numbers-with-repeated-digits.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    // pos=9 digits se kam
+//
+// pos,sum,tight
+// sum=8*9=72
+// int dp[pos][mask][tight]
+int dp[11][1024][2];
+int g(string s,int pos,int mask,int tight){
+    if(pos==s.size()){
+        return (mask!=0);
+    }
+    if(dp[pos][mask][tight]!=-1){
+        return dp[pos][mask][tight];
+    }
+    int res=0;
+    if(tight==1){
+        int limit=s[pos]-'0';
+        for(int i=0;i<=limit;i++){
+            if(mask&(1<<i)){
+                continue;
+            }
+            int newmask=(mask==0 and i==0? (mask):(mask|(1<<i)));
+            if(i==limit){
+                res+=g(s,pos+1,newmask,1);
+            }
+            else{
+                res+=g(s,pos+1,newmask,0);
+            }
+        }
+    }
+    else{
+        for(int i=0;i<=9;i++){
+            if(mask&(1<<i)){
+                continue;
+            }
+            int newmask=(mask==0 and i==0? (mask):(mask|(1<<i)));
+            res+=g(s,pos+1,newmask,0);
+        }
+    }
+    return dp[pos][mask][tight]=res;
+}
+    int numDupDigitsAtMostN(int n) {
+        memset(dp,-1,sizeof(dp));
+        return n-g(to_string(n),0,0,1);
+    }
+};
