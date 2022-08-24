acfc14b2472eacd724ac41989e8b3830ab93d933
Samandeep Singh <87420218+Higherorbit@users.noreply.github.com>
2022-08-24 20:15:35 +0530

    Time: 23 ms (84.22%), Space: 5.9 MB (72.69%) - LeetHub

---
+++ 326-power-of-three/326-power-of-three.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if(n==0){
+            return false;
+        }
+        while(n!=1){
+            if(n%3!=0){
+                return false;
+            }
+            n/=3;
+            if(n==0){
+                return false;
+            }
+            // cout<<n<<endl;
+        }
+        return true;
+    }
+};
