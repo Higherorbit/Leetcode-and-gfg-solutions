8b3da4aa5241ac7592dd28fa0c9be229651b60f2
Samandeep Singh <87420218+Higherorbit@users.noreply.github.com>
2022-09-11 15:23:40 +0530

    Time: 87 ms (23.35%), Space: 42.1 MB (6.35%) - LeetHub

--- 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
@@ -1,35 +1,28 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    void traverse(TreeNode* r, vector<int>&v){
-        if(r==NULL){
+    unordered_map<int,int>mp;
+    bool ans=false;
+    void traverse(TreeNode* root, int k){
+        if(root==NULL){
             return ;
         }
-        v.push_back(r->val);
-        traverse(r->left,v);
-        traverse(r->right,v);
+        if(mp.count((k-root->val))){
+            ans=true;
+            return ;
+        }
+        mp[root->val]++;
+        traverse(root->left,k);
+        traverse(root->right,k);
+        return ;
     }
     bool findTarget(TreeNode* root, int k) {
-        vector<int> v;
-        traverse(root,v);
-        set<int>s;
-        
-        for(int i=0;i<v.size();i++){
-            s.insert(v[i]);
-            if(s.count(k-v[i]) and (2*v[i]!=k)){
-                return true;
-            }
+        if(root==NULL){
+            return false;
         }
-        return false;        
+        ans=false;
+        mp.clear();
+        traverse(root,k);
+        return ans;
+        
     }
 };
