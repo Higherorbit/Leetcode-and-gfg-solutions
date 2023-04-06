f1696db8603c843a1c4e6b65d97ebec1136173fa
Samandeep Singh <87420218+Higherorbit@users.noreply.github.com>
2023-04-06 13:08:27 +0530

    Time: 50 ms (6.19%), Space: 27.6 MB (16.79%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int search(vector<int>& v, int t) {
+        int l=0,r=v.size()-1;
+        while(l<=r){
+            int mid=l+((r-l)>>1);
+            if(v[mid]==t){
+                return mid;
+            }
+            else if(v[mid]>t){
+                r=mid-1;
+            }
+            else{
+                l=mid+1;
+            }            
+        }
+        return -1;        
+    }
+};
