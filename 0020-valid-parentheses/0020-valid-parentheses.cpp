2bf113a6caab3fb6bd1a6f563f232e1086ac4896
Samandeep Singh <87420218+Higherorbit@users.noreply.github.com>
2023-04-10 09:49:33 +0530

    Time: 2 ms (46.33%), Space: 6.3 MB (56.25%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(auto i:s){
+            if(i=='(' or i=='[' or i=='{'){
+                st.push(i);
+            }
+            else{
+                if(st.empty() or (i==')' and st.top()!='(') or (i=='}' and st.top()!='{') or (i==']' and st.top()!='[')){
+                    return false;
+                }
+                else{
+                    st.pop();
+                }
+            }
+        }
+        return st.empty();
+    }
+};
