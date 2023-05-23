ad1730ffd194eab8a1c10809128e28e43829991c
Samandeep Singh <87420218+Higherorbit@users.noreply.github.com>
2023-05-23 12:34:32 +0530

    Time: 964 ms (5.04%), Space: 22.2 MB (5.28%) - LeetHub

---
+++ 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.cpp
@@ -0,0 +1,33 @@
+class KthLargest {
+public:
+    int k;
+    multiset<int>s;
+    KthLargest(int k, vector<int>& nums) {
+        this->k=k;
+        for(auto &x:nums){
+            s.insert(x);
+        }
+    }
+    int add(int val) {
+        //O(1);
+        // log(n)
+        // nlog(n)
+        // 1 2 3 4 5 n-k+1
+        s.insert(val);
+        int n=s.size();
+        int cnt=0;
+        for(auto &x:s){
+            cnt++;
+            if(cnt==n-k+1){
+                return x;
+            }
+        }
+        return -1;
+    }
+};
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest* obj = new KthLargest(k, nums);
+ * int param_1 = obj->add(val);
+ */
