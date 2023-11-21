60cc36d37ce7d02fc5a4c9f5966a05caa11ffdaf
Samandeep Singh <87420218+Higherorbit@users.noreply.github.com>
2023-11-21 18:07:16 +0530

    Time: 17 ms (91.23%), Space: 16 MB (64.30%) - LeetHub

---
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<int>parent;
+    int find(int x){
+        if(parent[x]==x){
+            return x;
+        }
+        return parent[x]=find(parent[x]);
+    }
+    int findCircleNum(vector<vector<int>>& v) {
+        int n=v.size();
+        parent.resize(n+1);
+        int ans=n;
+        for(int i=0;i<=n;i++) parent[i]=i;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(v[i][j]){
+                    int x=find(i+1),y=find(j+1);
+                    if(x!=y){
+                        parent[y]=x;
+                        ans--;
+                    }
+                }
+            }
+        }
+        return ans;
+        map<int,int>mp;
+        for(int i=1;i<=n;i++){
+            mp[parent[i]]++;
+        }
+        return mp.size();
+    }
+};
